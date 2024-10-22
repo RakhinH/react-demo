39930c980a7f2588544dd10c7d924fc410f6e9b7
RakhinH <rakhinchowdhury@icloud.com>
2024-10-22 15:26:46 +0100

    woo

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,11 +1,12 @@
-const Login = ({id, email}) => {
+const Login = ({id, email, password}) => {
 
 
     return ( 
         <>
-        <h1>lorem</h1>
+        <h1 className="text-3xl bg-body-tertiary">Log in</h1>
         <p>{id}</p>
         <p>{email}</p>
+        <p>{password}</p>
         </>
      );
 }
